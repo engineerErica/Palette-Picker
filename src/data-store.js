9075b1959ecb3fe37a2181c1ba9d4676aa785c29
engineerErica <ericaponce2002@gmail.com>
2023-11-02 20:09:56 +0000

    some js and html

--- src/data-store.js
+++ src/data-store.js
@@ -1,3 +1,26 @@
 import palettes from './palettes.json'
 
-export const getPalettes = () => palettes;
+export const setLocalStorageKey = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+export const getLocalStorageValue = (key) => {
+    try {
+        return JSON.parse(localStorage.getItem(key));
+    } catch (err) {
+        console.error(err);
+    }
+}
+export const getPalettes = () => getLocalStorageValue('palettes') || [];
+
+/*
+// Always return an array, either full of palettes or empty, this will make your downstream iterator functions a lot cleaner
+setPalettes(newPalettes)
+// Replace whatever palettes are saved in localStorage with
+initPalettesIfEmpty()
+// This one is important! If you don't have any palettes on page load, then you should add the default palettes to localStorage. To be clear, that's on page load, not the second they have 0 palettes. So if the user deletes each palette and then refreshes the page, suddenly the defaults will appear.
+addPalette(newPalette)
+// Add the palette to your saved localStorage palettes.
+removePalette(paletteUuid)
+// Remove the palette from your saved localStorage palettes as found by the palette's uuid
+*/
--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 import { v4 as uuidv4 } from 'uuid';
 import './style.css';
-// import { getPalettes } from './data-store'
+import { getPalettes } from './data-store'
 
 console.log('Hello World', uuidv4());
+console.log(getPalettes());
+
+const makeCard = (obj) => {
+    return `<div>${obj.palettetitle}</div>`;
+}
+
+const handleEvent = (e) => {
+    e.preventDefault();
+    const form = document.querySelector('#palette-form');
+    const formData = new FormData(form)
+    const obj = Object.fromEntries(formData)
+    console.log(obj.color2);
+    
+    const li = document.createElement('li');
+    li.innerHTML = makeCard(obj)
+    
+    const ul = document.querySelector('#palette-list');
+    
+    ul.append(li);
+}
+
+const button = document.querySelector('.button');
+
+button.addEventListener("click", handleEvent);
